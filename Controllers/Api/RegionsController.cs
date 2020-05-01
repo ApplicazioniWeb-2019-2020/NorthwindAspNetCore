cbc6cfba0a091500c4df1bd19d3e6c091ad562ef
Daniele <daniele.mantovani@algorithmedia.com>
2020-05-01 19:10:10 +0200

    API REST (CRUD)

---
+++ Controllers/Api/RegionsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NorthwindAspNetCore.Data;
+using NorthwindAspNetCore.Models;
+
+namespace NorthwindAspNetCore.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public RegionsController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Regions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Region>>> GetRegions()
+        {
+            return await _context.Regions.ToListAsync();
+        }
+
+        // GET: api/Regions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Region>> GetRegion(int id)
+        {
+            var region = await _context.Regions.FindAsync(id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            return region;
+        }
+
+        // PUT: api/Regions/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRegion(int id, Region region)
+        {
+            if (id != region.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(region).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Regions
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Region>> PostRegion(Region region)
+        {
+            _context.Regions.Add(region);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRegion", new { id = region.Id }, region);
+        }
+
+        // DELETE: api/Regions/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Region>> DeleteRegion(int id)
+        {
+            var region = await _context.Regions.FindAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            _context.Regions.Remove(region);
+            await _context.SaveChangesAsync();
+
+            return region;
+        }
+
+        private bool RegionExists(int id)
+        {
+            return _context.Regions.Any(e => e.Id == id);
+        }
+    }
+}

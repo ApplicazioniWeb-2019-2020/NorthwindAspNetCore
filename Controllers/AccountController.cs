63e9b7436b891f0d68d09240ef328d3f3f9cbfa8
Daniele <daniele.mantovani@algorithmedia.com>
2020-05-11 20:57:33 +0200

    Login, logout pages

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindAspNetCore.Models;
+using NorthwindAspNetCore.ViewModels;
+using System.Threading.Tasks;
+
+namespace NorthwindAspNetCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<SiteUser> _signInManager;
+
+        public AccountController(SignInManager<SiteUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, [FromQuery(Name = "ReturnUrl")] string returnUrl)
+        {
+            if (!ModelState.IsValid) return View();
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+            {
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return Redirect(returnUrl);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid UserName or Password");
+
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}

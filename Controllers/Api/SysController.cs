07b731091b7c67191f507099f69ee6e978247815
Daniele <dmantovani73@gmail.com>
2020-05-26 18:44:22 +0200

    Deploy

---
+++ Controllers/Api/SysController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NorthwindAspNetCore.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SysController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetInfo() => Environment.OSVersion.Platform.ToString();
+    }
+}
